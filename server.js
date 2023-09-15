a17f9da20ae4c74fa9181b09b10a03b3793b8913
geekpuja <pujuagarwal5263@gmail.com>
2023-09-15 21:25:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const app = express();
+const port = 3000; // Adjust the port as needed
+
+app.use(express.json());
+
+app.post('/generate-pdf', async (req, res) => {
+  const { data } = req.body;
+
+  // Read the Handlebars template file
+  const templateSource = fs.readFileSync('template.hbs', 'utf8');
+
+  // Compile the Handlebars template
+  const template = handlebars.compile(templateSource);
+
+  // Render the HTML content with the data
+  const html = template(data);
+
+  // Create a new Puppeteer browser instance
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Set the HTML content of the page
+  await page.setContent(html);
+
+  // Generate PDF from the HTML content
+  const pdfBuffer = await page.pdf();
+
+  // Close the browser
+  await browser.close();
+
+  // Set response headers to indicate a PDF file
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename=report.pdf');
+
+  // Send the generated PDF as the response
+  res.send(pdfBuffer);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
